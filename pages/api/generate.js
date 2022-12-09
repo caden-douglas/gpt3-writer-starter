b07a5bdf8ac136508030f4908927abf6d1cc4f27
Joseph Douglas <joecdouglas2@gmail.com>
2022-12-09 23:27:01 +0000

    Changes to be committed: 	deleted:    .env.example     modified:   package-lock.json 	modified:   package.json 	new file:   pages/api/generate.js 	modified:   pages/index.js 	new file:   yarn.lock

---
+++ pages/api/generate.js
@@ -0,0 +1,56 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix =
+`
+give me a list of 3 meals based on the ingredients below. 
+
+ingredients: 
+`
+
+const generateAction = async (req, res) => {
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.8,
+    max_tokens: 700,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  // I build Prompt #2.
+  const secondPrompt = 
+  `take the meals and generate a recipe based on ingredients below. on the first line include the meal and the macros. then skip a line and list the ingredients. finally, skip another line and explain the preperation steps in detail.
+  
+  ingredients: ${req.body.userInput}
+
+  meals: ${basePromptOutput.text}
+
+  meal ideas: 
+  `
+  
+  // I call the OpenAI API a second time with Prompt #2
+  const secondPromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${secondPrompt}\n`,
+    // I set a higher temperature for this one. Up to you!
+    temperature: 0.85,
+		// I also increase max_tokens.
+    max_tokens: 1250,
+  });
+  
+  // Get the output
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+  // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+  res.status(200).json({ output: secondPromptOutput });
+};
+
+export default generateAction;
