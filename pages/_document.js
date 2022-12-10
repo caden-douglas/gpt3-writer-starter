c2a74f443c98025eb6848a81feafc3f971a40803
Joseph Douglas <joecdouglas2@gmail.com>
2022-12-10 01:29:38 +0000

    Changes to be committed: 	modified:   pages/_document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="My Meal Wizard" key="title"/>
+        <meta property="og:description" content="built with buildspace" key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jphttps://thenounproject.com/api/private/icons/657180/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=752&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0&token=gAAAAABjk9fKL1bKiYQfhXOu3DYbXuTjDF_-7KdvwpTDPws5EPw-py25S35CgEFuHIWxU_AwPSgadx-roqVM21ZklrDJbHv3yg%3D%3D"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
